04377f690e9d91733b8da551e71e94be49a58bf1
Matt Grogan <johnmattgrogan@gmail.com>
2015-10-09 16:28:50 -0500

    updated feasible region on chart

--- pred400/week3/dicussion_question.py
+++ pred400/week3/dicussion_question.py
@@ -9,30 +9,30 @@ import numpy as np
 x = np.arange(-2,10,0.1)
 y = np.arange(-2,10,0.1)
 
-# y1 = -0.33*x + 2
-# y2 = -2.0*x + 3
+y1 = -0.33*x + 2
+y2 = -2.0*x + 3
 
-d1 = -3.0*x + 2
-d2 = -0.5*x + (3.0/2)
+# d1 = -3.0*x + 2
+# d2 = -0.5*x + (3.0/2)
 
 y3= 10+0.0*x
 
 z = -10+0.0*x
 
-# y4=[0]*len(x)
-# for k in range(0,len(x)):
-#     if y1[k] >= y2[k]:
-#         y4[k]=y1[k]
-#     if y2[k] > y1[k]:
-#         y4[k]=y2[k]
-
-
-y6=[0]*len(x)
+y4=[0]*len(x)
 for k in range(0,len(x)):
-    if d1[k] >= d2[k]:
-        y6[k]= d2[k]
-    if d2[k] > d1[k]:
-        y6[k]= d1[k]
+    if y1[k] >= y2[k]:
+        y4[k]=y1[k]
+    if y2[k] > y1[k]:
+        y4[k]=y2[k]
+
+
+# y6=[0]*len(x)
+# for k in range(0,len(x)):
+#     if d1[k] >= d2[k]:
+#         y6[k]= d2[k]
+#     if d2[k] > d1[k]:
+#         y6[k]= d1[k]
 
 
 
@@ -50,23 +50,30 @@ plt.grid(True)
 
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
-plt.title('The shaded area is the feasible region for the maximization problem.')
+plt.title('The shaded area is the feasible region for the minimization problem.')
 
-# plt.plot(x, y1, color='b')
-# plt.plot(x, y2, color='b')
+plt.plot(x, y1, color='b')
+plt.plot(x, y2, color='b')
 
-plt.plot(x, d1, color='g')
-plt.plot(x, d2, color='g')
+# plt.plot(x, d1, color='g')
+# plt.plot(x, d2, color='g')
 
-plt.plot(x,z1,'k--')
+plt.plot(x,y5,'k--')
 
-# plt.scatter(.6, 1.8, c='red', s=40)
-plt.scatter(.2, 1.4, c='red', s=40)
+plt.scatter(.6, 1.8, c='red', s=40)
+# plt.scatter(.2, 1.4, c='red', s=40)
 
-# plt.legend(['x+3y >= 6','2x+y >= 3', '5.4 = 3x+2y'], 'best')
-plt.legend(['x+2y <= 3','3x+y <= 2', '5.4 = 6x+3y'], 'best')
+plt.legend(['x+3y >= 6','2x+y >= 3', '5.4 = 3x+2y'], 'best')
+# plt.legend(['x+2y <= 3','3x+y <= 2', '5.4 = 6x+3y'], 'best')
 
 # plt.fill_between(x,y3,y4, color='grey',alpha=0.2)
-plt.fill_between(x,z,y6, color='yellow',alpha=0.2)
+
+# x= [0.0, 0.0,0.2, .66 ]
+# y= [0.0, 1.5,1.4,   0 ]
+
+xx= [.6, 0.0,    0,   10.1, 10.1, 6]
+yy= [1.8,  3, 10.1,   10.1,    0, 0]
+# plt.fill_between(x,z,y6, color='yellow',alpha=0.2)
+plt.fill(xx,yy, color='grey')
 
 plt.show()
