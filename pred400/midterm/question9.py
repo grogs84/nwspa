7ddcf963b63cac297d600397cc0ea701c2dbf5f5
Matt Grogan <johnmattgrogan@gmail.com>
2015-10-22 21:28:05 -0500

    finishing touches on question 9

--- pred400/midterm/question9.py
+++ pred400/midterm/question9.py
@@ -22,6 +22,7 @@ pw_or_pb = .96
 # p(A & B) = P(A) + P(B) - P(A or B)
 print "p(A & B) = P(A) + P(B) - P(A or B)"
 print "p(w & b) = P(w) + p(b) - P(w or b)"
+print "p(w & b) = {} + {} - {}".format(pw,pb,pw_or_pb)
 pw_a_pb = pw + pb - pw_or_pb
 print "p(w & b) = {}".format(pw_a_pb)
 print '\n'
@@ -29,18 +30,20 @@ print '\n'
 # P(B|A) = P(A & B)/P(A)
 print "P(B|A) = P(A & B)/P(A)"
 print "P(b|w) = p(w & b)/P(w)"
+print "P(b|w) = {} / {}".format(pw_a_pb,pw)
 pb_given_w = float(pw_a_pb) / pw
 print "P(b|w) = {}".format(pb_given_w)
 print '\n'
 
 # P(b|w') = 1 - P(b|w)
 print "P(b|w') = 1 - P(b|w)"
+print "P(b|w') = 1 - {}".format(pb_given_w)
 pb_given_nw = 1.0 - pb_given_w
 print "P(b|w') = {}".format(pb_given_nw)
 print '\n'
 
 print "P(A|B) = P(A)*P(B|A)/ P(A)*(B|A) + P(A')*P(B|A')"
 print "P(w|b) = P(w)*P(b|w)/ P(w)*P(b|w)+ P(w')*P(b|w')"
-
+print "P(w|b) = {}*{}/ ({}*{})+({}*{})".format(pw,pb_given_w,pw,pb_given_w,npw,pb_given_nw)
 pw_given_b = float(pw*pb_given_nw)/((pw*pb_given_nw)+(npw*pb_given_nw))
 print "P(w|b) = {}".format(pw_given_b)
