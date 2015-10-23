9bfb6cc95987dd85e0836042a518e12fffd3aec9
Matt Grogan <johnmattgrogan@gmail.com>
2015-10-23 11:33:04 -0500

    added questions 1-7 to word file

--- pred400/midterm/question1.py
+++ pred400/midterm/question1.py
@@ -13,11 +13,12 @@ fixed_cost = 90
 # marginal_rev = 7*x
 marginal_rev = 7
 
-
 remainder = 90%3.5
 
 break_even = 90//(7.0 - 3.5)
 
+# you can't sell fractions of a shirt. 
+# You have to round up if there is a remainder.
 if remainder > 0: break_even += 1
 
 def cost(x):
@@ -26,12 +27,19 @@ def cost(x):
 def revenue(x):
 	return marginal_rev*x
 
+plt.xlim(0,60)
+plt.ylim(0,300)
+plt.xlabel('Sales')
+plt.ylabel('$ Amount')
+
 
 plt.plot(x,cost(x))
 plt.plot(x,revenue(x))
 plt.scatter(break_even, 7*break_even)
+plt.legend(['Total Cost: 3.5x + 90','Marginal Reveune: 7x=y'],loc='best')
 
 plt.show()
 
+print 'They must sell {} to break even'.format(break_even)
 
 
