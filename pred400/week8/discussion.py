55213300caeb22b11730c16f02bdba1d0d4f6295
Matt Grogan <johnmattgrogan@gmail.com>
2015-11-13 16:30:06 -0600

    week8 discusion tweaks

--- pred400/week8/discussion.py
+++ pred400/week8/discussion.py
@@ -1,3 +1,4 @@
+######################################################################################################
 # The fundamental thorem of calculus brings together the two subjects of defferentitaion and interation
 # The theorm says F(x) = intergral (x-a) f(t)dt
 # the way i think about it is on the interval [a,b] the derivative of F(x) is 
@@ -12,23 +13,24 @@
 #
 # Generally we need to forecast our projects by month before a project manager has built 
 # a detailed monthly budget. One way to solve this problem is take the project budget and 
-# divide it by the duration of the projects in months. month spend = Budget/# of Months
+# divide it by the duration of the projects in months. month spend = budget/# of months
 #
-# This works, but not great. Projects tend to spend more less and more slowly during design, then 
+# This works, but not great. Projects tend to spend less and more slowly during design, then 
 # as construction starts increases quickly, and finally drops to almost zero during closeout. If the
-# costs as spread evenly in the budget it will seem as though projects are in design, blowing the budget in
-# construction and under spending again in closeout. 
+# costs are spread evenly in the budget it will seem as though projects in design are under spending,
+# projects in construction are blowing the budget, and under spending again in closeout. 
 #
 # One way to create a more accurate monthly estimate for project is to model it after completed projects 
-# monthly spend. To do this we fit a polynomial function to a series of points (x, y) using excel where:
+# monthly spend that we think behaved normally. To do this we fit a polynomial function to a series of 
+# points (x, y) using excel where:
 #
-# x = % complete a project is (month of spend/# of months)
-# y = % of project spend (monthly spend / total project cost)
+# x = % complete a project is (month of spend/# of months for project_i) for each i
+# y = % of project spend (monthly spend / total project cost for project_i) for each i
 #
 #
 # After some tweaking and normalizing I have a function f(t) and the definite intergral (1-0) f(t)dt = 1.
 # To find the monthly cashflows you would sum up f(month/months)*(budget/months). Unfortunetly, because the number of 
-# months is sometimes small this sum will be less than the buget. As the number of months approces infinity the sum
+# months is sometimes small this sum will be less than the buget. As the number of months approches infinity the sum
 # approaches the budget. Try as we might we can't increase the number of months to completion to infinity.
 #
 # To get around this we use the fundamental theorm of calculus. It stats that (b to a)f(t)dt. = F(b) - F(a)
@@ -40,7 +42,7 @@
 #
 #
 #
-##
+############################################################################################################
 import cPickle as pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -56,7 +58,7 @@ x0 =  0.042690311559
 gg = np.poly1d([x3,x2,x1,x0])
 
 
-budget = 1
+budget = 378000
 months = 18 + 1
 xs = np.array([float(month)/months for month in range(months)])
 ys = np.array([g(x)*(float(budget)/months) for x in xs])
@@ -73,11 +75,19 @@ Fg = g.integ()
 
 print Fg(1)
 
-ys = np.array([(Fg(x+1./months)*budget)-(Fg(x)*budget) for x in xs])
+ys1 = np.array([(Fg(x+1./months)*budget)-(Fg(x)*budget) for x in xs])
 
-print ys
-print sum(ys)
+print sum(ys - ys1)
+print sum(ys1)
 
 plt.figure()
-plt.plot(xs,ys)
+plt.bar(xs,ys1,width=(1.0/months *.85))
+plt.plot(xs,ys1,c='r')
+plt.show()
+
+ys2 = np.array([(Fg(x)*budget) for x in xs])
+
+plt.figure()
+plt.bar(xs,ys2,width=(1.0/months *.85))
+plt.plot(xs,ys2,c='r')
 plt.show()
